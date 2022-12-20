3c40bb18ccb2b2c923d4c401397d12a3eb987646
Polina <106979919+kollarbone@users.noreply.github.com>
2022-12-20 17:21:33 +0400

    first page

---
+++ src/subComponents/HomeButton.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Power = styled.button`
+  position: fixed;
+  top: 2rem;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: ${(props) => props.theme.body};
+  padding: 0.3rem;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 3;
+  cursor: pointer;
+  & > *:first-child {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+const PowerButton = () => {
+  return (
+    <Power>
+      <NavLink to="/">
+        <motion.h3
+          initial={{
+            y: -200,
+            transition: { type: "spring", duration: 1.5, delay: 1 }
+          }}
+          animate={{
+            y: 0,
+            transition: { type: "spring", duration: 1.5, delay: 1 }
+          }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Home
+        </motion.h3>
+      </NavLink>
+    </Power>
+  );
+};
+
+export default PowerButton;
---
+++ src/subComponents/LogoComponent.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { DarkTheme } from "../components/Themes";
+
+const Logo = styled.h1`
+  display: inline-block;
+  color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+  font-family: "Pacifico", cursive;
+  position: fixed;
+  left: 2rem;
+  top: 1.5rem;
+  z-index: 3;
+`;
+
+const LogoComponent = (props) => {
+  return <Logo color={props.theme}>PS</Logo>;
+};
+
+export default LogoComponent;
