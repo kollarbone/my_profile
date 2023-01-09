96dbf86eecc77591b3334f96b9e77d076800b8a2
Polina <106979919+kollarbone@users.noreply.github.com>
2023-01-09 14:31:43 +0400

    new

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -51,5 +51,14 @@ export const Work = [
 
     demo: "https://ble8lt.csb.app/",
     github: "https://github.com/kollarbone/react_app_popup"
+  },
+  {
+    id: 5,
+    name: "Art gallery",
+    description: "Gallery of different arts with filters",
+    tags: ["react", "styledComponents", "axios", "framer-motion"],
+
+    demo: "https://ngkkjc.csb.app",
+    github: "https://github.com/kollarbone/gallery"
   }
 ];
